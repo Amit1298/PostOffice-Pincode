d107c27d88c034a09bded06595ea8a969841c677
Amit <amitkr24031995@gmail.com>
2021-10-09 18:42:25 +0530

    Adding Pincode

---
+++ UC1simplePincode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UC1simplePincode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Pin;
+            bool result;
+            SimplePinValidate simplepinvalidate = new SimplePinValidate();
+            Console.WriteLine("Enter Pincode: ");
+            Pin = Console.ReadLine();
+            result = simplepinvalidate.simplePin(Pin);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ UC1simplePincode/SimplePinValidate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC1simplePincode
+{
+    class SimplePinValidate
+    {
+        public string RagexPattern = "^[0-9]{6}$";
+        public bool simplePin(string pincode)
+        {
+            return Regex.IsMatch(pincode, RagexPattern);
+        }
+    }
+}
